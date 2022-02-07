0bc520809be3cc3016f8a1314d48994d58c129ee
blondbond69 <daniel.bilek1@gmail.com>
2022-02-07 20:28:38 +0100

    First step. Create function computerPlay.

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-//Rock Paper Scissors Game 
-// CREATE three variables: rock paper scissors THEN
-// CREATE function: computerTurn WHICH randomly pick one of variables and return it  
- 
- 
+// CREATE function getId that randomly select number from 1 to 3
+
+function getId() {
+    return Math.floor(Math.random() * 3) + 1;  
+}
+// console.log(getId()); 
+
+// CREATE function computerPlay that return string depending of the drawn number from 1 to 3 
+
+function computerPlay() {
+    const currentId = getId(); 
+    if (currentId === 1) {
+        return "rock";
+    } else if (currentId === 2) {
+        return "paper";
+    } else 
+        return "scissors"; 
+}
+
+computerPlay(); 
+
+//console.log(computerPlay()); 
+
+//CREATE function singleRound that contains one round of RPS game.
+
 
 
